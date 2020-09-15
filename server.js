e2a9f129fbe8fb0d276bed23ac4b5107679b54ba
CannedShroud <hypersync112@gmail.com>
2020-09-15 15:58:52 +0530

    Setup database and create routes

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // importing
 import express from "express";
 import mongoose from "mongoose";
+import dbMessages from "./dbMessages.js";
+import Messages from "./dbMessages.js";
 
 // app config
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 4000;
 
 // middlewares
+app.use(express.json());
 
 // DB config
 const connectionURL =
@@ -22,6 +25,28 @@ mongoose.connect(connectionURL, {
 // api routes
 app.get("/", (req, res) => {
   res.status(200).send("Hello World");
+});
+
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
 });
 
 // listener
